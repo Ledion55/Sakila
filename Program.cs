d17a9b36e6860ced094e95db4852e0e6efdc1477
Ledion55 <ledion.selmani1@yaelev.se>
2024-12-11 09:12:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ADO.netUppgiften
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Sakila;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            Console.WriteLine("Ange skådespelarens förnamn:");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Ange skådespelarens efternamn:");
+            var lastName = Console.ReadLine();
+
+            var query = @"
+                SELECT film.title 
+                FROM film
+                INNER JOIN film_actor ON film.film_id = film_actor.film_id
+                INNER JOIN actor ON film_actor.actor_id = actor.actor_id
+                WHERE actor.first_name = @FirstName AND actor.last_name = @LastName";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FirstName", firstName.ToUpper());
+            command.Parameters.AddWithValue("@LastName", lastName.ToUpper());
+
+
+            connection.Open();
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                Console.WriteLine($"{firstName} {lastName} har deltagit i dessa filmer:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["title"]);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Inga filmer hittades för {firstName} {lastName}.");
+            }
+
+            connection.Close();
+        }
+
+    }
+}
